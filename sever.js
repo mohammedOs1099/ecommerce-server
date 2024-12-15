1b0de2ead5fa93600512e331270d4d4b8fe6a1b6
mohammedOs1099 <mohammed.osama1099@gmail.com>
2024-12-15 17:13:13 +0200

    vercel update2

--- sever.js
+++ sever.js
@@ -2,11 +2,8 @@ import jsonServer from "json-server";
 
 const server = jsonServer.create();
 const router = jsonServer.router({
-  books: [
-    { id: 1, title: "Book 1", author: "Author 1" },
-    { id: 2, title: "Book 2", author: "Author 2" }
-  ]
-});
+  books: []
+}); // قاعدة بيانات في الذاكرة
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
